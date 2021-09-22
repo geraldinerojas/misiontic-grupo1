9422ac7be9a7d8f2fa9aa5873d8d81df366d63c5
Elag1988 <acevedoedgard1988@msn.com>
2021-09-21 21:43:31 -0500

    Codigo para revision

---
+++ DomiciliosProductos2/src/main/java/Listar/DatosProducto.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Listar;
+
+/**
+ *
+ * @author aceve
+ */
+public class DatosProducto {
+                // Número de producto
+    int ID;
+    // nombre del producto
+    String nombre;
+    // precio de los productos básicos
+    double precioDeVenta;
+    //precio de compra 
+    double precioDeCompra;
+    // número de productos
+    private int stock;
+    // Precio total
+    private double total;
+    // Precio Compra total
+    
+    private double totalCompra;
+}
